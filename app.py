53a3a222636c908fe40c5215a7a02925953fe339
Kamil Przebinda <sarah.smith@email.com>
2022-07-29 15:31:42 +0200

    Delete

--- app.py
+++ app.py
@@ -8,10 +8,5 @@ def home():  # put application's code here
 	return 'Hello TEST2!'
 
 
-@app.route('/login`')
-def login():  # put application's code here
-	return 'Login'
-
-
 if __name__ == '__main__':
 	app.run()
