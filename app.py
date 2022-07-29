a82778f2aa9aaaf5143164d3f599ddf408a2ff9f
Kamil Przebinda <sarah.smith@email.com>
2022-07-29 15:24:10 +0200

    Test add login method

--- app.py
+++ app.py
@@ -4,8 +4,13 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def home():  # put application's code here
 	return 'Hello TEST2!'
+
+
+@app.route('/login`')
+def login():  # put application's code here
+	return 'Login'
 
 
 if __name__ == '__main__':
